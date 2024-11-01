854d211ba4e2898fe1a2ea488cc1d70c67e142d2
Dea <deaaa.putri.1995@gmail.com>
2024-11-01 14:38:02 +0800

    fix bugs host value

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Hallo kita akan membuat RESTful API');
+const Hapi = require('@hapi/hapi');
+const routes = ('./routes');
+
+const init = async () => {
+const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+})
+};
